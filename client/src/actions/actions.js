e05313930d867743674263f63aaa3195fe96f082
CodeforLiberty <sswell@live.com>
2017-02-22 15:31:59 -0500

    Made plan starting to implement Redux

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -1,1 +1,29 @@
-import store from '../store'
+import thunk from 'react-thunk';
+import store from '../store';
+
+export const GOTNEXTQUESTION = "GETTING_NEXT_QUESTION"
+export function gotNextQuestion (question) {
+	return {
+		type:GOTNEXTQUESTION,
+		question:question.question,
+		options:question.options,
+		Answer:question.correctAnswer,
+
+	}
+}
+
+//dispatch synchronous action when completed
+export function getNextQuestion(index) {
+	return function(dispatch) {
+	const url = '/questions'+ index;
+
+	return fetch(url).then(response => {
+			
+			return response.json();
+		}).then(question => {
+			return dispatch(gotNextQuestion(question));
+			 
+		})
+	}
+
+	module.exports = {GOTNEXTQUESTION, getNextQuestion, gotNextQuestion};
