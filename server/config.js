f02f7b75dafa430a7e82e730d9a46dd5afe21666
Alex M <aljmiller87@gmail.com>
2017-03-29 21:15:06 -0400

    updated config for deploy

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 const HOST = process.env.SERVER_HOST || process.env.HOST || 'localhost';
 const PORT = process.env.SERVER_PORT || process.env.PORT || 8080;
-const ROOT = `http://${HOST}:${PORT}`;
+const ROOT = process.env.SERVER_ROOT || `http://${HOST}:${PORT}`;
 const CLIENT_HOST = process.env.CLIENT_HOST || 'localhost';
 const CLIENT_PORT = process.env.CLIENT_PORT || 3000;
 const CLIENT_ROOT = `http://${CLIENT_HOST}:${CLIENT_PORT}`;
